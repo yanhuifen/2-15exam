a48abac923687c4f989373dcd00cf5794a53824e
yhf <1508135811@qq.com>
2019-02-15 09:21:31 +0800

    2-15jineng

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+var data = [{
+        "title": "新闻哥1：Dopa竟为厂长直播新闻哥1：Dopa竟为厂长直播新闻哥1：Dopa竟为厂长直播新闻哥1：Dopa竟为厂长直播？",
+        "img": "img/li1.PNG"
+    },
+    { "title": "新闻哥q：Dopa竟为厂长直播新闻哥q：Dopa竟为厂长直播新闻哥q：Dopa竟为厂长直播？", "img": "img/li2.PNG" },
+    { "title": "新闻哥w：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播？", "img": "img/li1.PNG" },
+    { "title": "新闻哥e：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播？", "img": "img/li2.PNG" },
+    { "title": "新闻哥r：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播？", "img": "img/li1.PNG" },
+    { "title": "新闻哥t：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播新闻哥w：Dopa竟为厂长直播？", "img": "img/li2.PNG" }
+]
